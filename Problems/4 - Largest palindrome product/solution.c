d5427a19cfded21bf9dced25c738138e40d1134a
Gabriel Jestin <jestingabriel@gmail.com>
2020-02-11 15:57:49 +0100

    Problem 4 solved

---
+++ Problems/4 - Largest palindrome product/solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int reverse_integer(int);
+int is_palindrome(int);
+
+int main(void) {
+    int res = 0;
+    for (int i = 999; i > 99; i--) {
+        for (int j = i; j > 99; j--) {
+            if (is_palindrome(i*j)) {
+                if (i*j > res) {
+                    res = i*j;
+                }
+            }
+        }
+    }
+    printf("Result: %d\n", res);
+}
+
+int reverse_integer(int number) {
+    int reverse = 0;
+    int last_digit = 0;
+    while (number > 0) {
+        last_digit = number % 10;
+        reverse = reverse*10 + last_digit;
+        number = number/10;
+    }
+    return reverse;
+}
+
+int is_palindrome(int number) {
+    return (number == reverse_integer(number));
+}
